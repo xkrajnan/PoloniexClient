791d40ecc181d4fb35b3d72a235fe01008bdc4c7
xkrajnan <michal.krajnansky@gmail.com>
2016-06-12 15:37:45 +0200

    initial commit

---
+++ src/test/java/xkrajnan/trading/poloniex/PoloniexClientTest.java
@@ -0,0 +1,38 @@
+package xkrajnan.trading.poloniex;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PoloniexClientTest extends TestCase
+{
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public PoloniexClientTest(String testName)
+	{
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(PoloniexClientTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp()
+	{
+		assertTrue(true);
+	}
+}
